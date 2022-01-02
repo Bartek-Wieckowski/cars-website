078a853b32d5a3a83e03be9d4d07f5d13912c683
Bartek Wieckowski <bart.it.pro@gmail.com>
2022-01-02 13:52:56 +0100

    complete video section html,css,js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ $(".target").each(function() {
     $( this ).toggleClass("change");
 });
 })
+
+$(".video-element").each(function(){
+    $(this).mouseenter(function(){
+        this.play()
+    });
+    $(this).mouseout(function(){
+        this.pause()
+    })
+})
